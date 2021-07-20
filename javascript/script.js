e16cc5c7bc2bc5393eb037efa8cbf73ffd0f08ef
Mariam-Kabu <mariamkabulovi@gmail.com>
2021-07-20 19:20:10 +0400

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// main page first slider
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -22,26 +23,42 @@ function currentSlide(n) {
     dots[slideIndex-1].className += " active";
   }
 
-  var Index = 1;
-  displaySlides(Index);
+
+// main page - double slider
+var Index = 1;
+displaySlides(Index);
+
+// Thumbnail image controls
+function currentPage(a) {
+    displaySlides(Index = a);
+  }
   
-  // Thumbnail image controls
-  function currentPage(a) {
-      displaySlides(Index = a);
+  function displaySlides(a) {
+    var x;
+    var slide = document.getElementsByClassName("douSlides");
+    var dot = document.getElementsByClassName("douDot");
+    if (a > slide.length) {Index = 1}
+    if (a < 1) {Index = slide.length}
+    for (x = 0; x < slide.length; x++) {
+        slide[x].style.display = "none";
     }
-    
-    function displaySlides(a) {
-      var x;
-      var slide = document.getElementsByClassName("douSlides");
-      var dot = document.getElementsByClassName("douDot");
-      if (a > slide.length) {Index = 1}
-      if (a < 1) {Index = slide.length}
-      for (x = 0; x < slide.length; x++) {
-          slide[x].style.display = "none";
-      }
-      for (x = 0; x < dot.length; x++) {
-          dot[x].className = dot[x].className.replace(" activate", "");
-      }
-      slide[Index-1].style.display = "block";
-      dot[Index-1].className += " activate";
+    for (x = 0; x < dot.length; x++) {
+        dot[x].className = dot[x].className.replace(" activate", "");
     }
+    slide[Index-1].style.display = "block";
+    dot[Index-1].className += " activate";
+  }
+
+
+// company page
+const year = document.querySelectorAll('.year');
+year.forEach(function(date, key){
+  date.addEventListener('click', function(){
+    date.classList.add("opend");
+    year.forEach(function(time, Index){
+      if (key != index){
+        time.classList.remove('opend');
+      }
+    })
+  })
+})
