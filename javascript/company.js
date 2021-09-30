edf022ea76ede997024f4428df695d7cbb35a8ba
Mariam-Kabu <mariamkabulovi@gmail.com>
2021-10-01 00:02:21 +0400

    responsive and my office page

--- javascript/company.js
+++ javascript/company.js
@@ -1,3 +1,44 @@
+// overlay menu - responsive
+const menuBar = document.querySelector('.toggle-bar');
+const overlayMenu = document.querySelector('.overlay-menu');
+const overlayLogo =document.querySelector('.toggle-logo');
+let menuOpen = false;
+menuBar.addEventListener('click', ()=>{
+  if(!menuOpen){
+    overlayMenu.classList.add('open');
+    overlayMenu.classList.remove('closed');
+    menuOpen = true;
+  }
+});
+overlayLogo.addEventListener('click', ()=>{
+  if(menuOpen){
+    overlayMenu.classList.add('closed');
+    overlayMenu.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const parentList = document.querySelectorAll(".overlay-li");
+parentList.forEach(div => {
+  var presentList = div.querySelector(".oli-div");
+  var hiddenList = div.querySelector(".child-oul");
+  var arrow = div.querySelector(".oli-arrow");
+  let childList = false;
+  presentList.addEventListener('click', ()=>{
+    if(hiddenList.style.display == 'block'){
+      hiddenList.style.display = 'none';
+      childList = false;
+      arrow.classList.remove('dropDown');
+    }else{
+      hiddenList.style.display = 'block';
+      childList = true;
+      arrow.classList.add('dropDown');
+    }
+  })
+});
+
+
+// company page
 const divParent = document.querySelectorAll(".ddi-parent");
 divParent.forEach(div => {
     var presentChild = div.querySelector(".drop-down-info-container");
--- javascript/script.js
+++ javascript/script.js
@@ -1,41 +1,63 @@
-//burger bar overlay manu
+// overlay menu - responsive
+const menuBar = document.querySelector('.toggle-bar');
+const overlayMenu = document.querySelector('.overlay-menu');
+const overlayLogo =document.querySelector('.toggle-logo');
+let menuOpen = false;
+menuBar.addEventListener('click', ()=>{
+  if(!menuOpen){
+    overlayMenu.classList.add('open');
+    overlayMenu.classList.remove('closed');
+    menuOpen = true;
+  }
+});
+overlayLogo.addEventListener('click', ()=>{
+  if(menuOpen){
+    overlayMenu.classList.add('closed');
+    overlayMenu.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
 const parentList = document.querySelectorAll(".overlay-li");
 parentList.forEach(div => {
   var presentList = div.querySelector(".oli-div");
   var hiddenList = div.querySelector(".child-oul");
+  var arrow = div.querySelector(".oli-arrow");
+  let childList = false;
   presentList.addEventListener('click', ()=>{
     if(hiddenList.style.display == 'block'){
       hiddenList.style.display = 'none';
+      childList = false;
+      arrow.classList.remove('dropDown');
     }else{
       hiddenList.style.display = 'block';
+      childList = true;
+      arrow.classList.add('dropDown');
     }
   })
 });
 
-// main page first slider
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+// main page first slider
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
 
 
 // main page - double slider
